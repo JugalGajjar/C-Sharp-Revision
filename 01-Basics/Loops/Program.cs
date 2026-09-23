45b3263793e78d2cac134987f47f2aa2854bbf5d
Jugal Gajjar <812jugalgajjar@gmail.com>
2026-09-23 13:29:38 -0400

    Add loops

---
+++ 01-Basics/Loops/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("For Loop:");
+for (int i = 0; i < 3; i++) {
+    Console.WriteLine($"Iteration {i}");
+}
+
+Console.WriteLine();
+Console.WriteLine("While Loop:");
+
+int counter = 0;
+while (counter < 3) {
+    Console.WriteLine($"Counter: {counter}");
+    counter++;
+}
+
+Console.WriteLine();
+Console.WriteLine("Do-While Loop:");
+
+counter = 0;
+do {
+    Console.WriteLine($"Counter: {counter}");
+    counter++;
+} while (counter < 3);
