3c1906feb8d001342e40082b0e6debce36ea0260
Jugal Gajjar <812jugalgajjar@gmail.com>
2026-09-23 12:53:24 -0400

    Add operators

---
+++ 01-Basics/Operators/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 10;
+int b = 5;
+
+int sum = a + b;
+int difference = a - b;
+int product = a * b;
+int quotient = a / b;
+int remainder = a % b;
+
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Difference: {difference}");
+Console.WriteLine($"Product: {product}");
+Console.WriteLine($"Quotient: {quotient}");
+Console.WriteLine($"Remainder: {remainder}");
+
+int power = (int)Math.Pow(a, b);
+Console.WriteLine($"Power: {power}");
+
+double sqrtA = Math.Sqrt(a);
+Console.WriteLine($"Square Root of a: {sqrtA}");
+double sqrtB = Math.Sqrt(b);
+Console.WriteLine($"Square Root of b: {sqrtB}");
